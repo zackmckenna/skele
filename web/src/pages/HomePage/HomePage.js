6727aec97996c03ed736b77c046415b1d85ca837
Zack McKenna <zackmckenna@gmail.com>
2020-09-02 16:31:17 -0400

    created remote repo

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -2,23 +2,14 @@ import { useState, useEffect } from 'react'
 import { Link, routes } from '@redwoodjs/router'
 import socketClient from 'socket.io-client'
 const ENDPOINT = 'http://localhost:3000/'
+const socket = socketClient(ENDPOINT)
 
 const HomePage = () => {
-  const [response, setResponse] = useState('')
-
-  useEffect(() => {
-    const socket = socketClient(ENDPOINT)
-    socket.on('test', (data) => {
-      setResponse(data)
-    })
-
-    return () => socket.disconnect()
-  }, [])
-
   return (
     <>
       <div>
         <h1 className="p3">Skele</h1>
+        <h2>welcome to skel</h2>
       </div>
     </>
   )
